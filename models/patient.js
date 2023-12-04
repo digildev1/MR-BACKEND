543fe9bd4a84ddef1b4f1552ed79ca7ef6755ad3
digildev1 <152153973+digildev1@users.noreply.github.com>
2023-12-04 13:36:15 +0530

    updated string to number

--- models/patient.js
+++ models/patient.js
@@ -11,7 +11,7 @@ const PatientSchema = new mongoose.Schema({
     PatientType: String,
 
     Repurchase: [{
-        DurationOfTherapy: String,
+        DurationOfTherapy: Number,
         TotolCartiridgesPurchase: String,
         DateOfPurchase: Date,
         TherapyStatus: String,
